c1fafffb64325ef728642e2c3a1588a74150a750
Aidan <aidankmcalister@gmail.com>
2023-12-19 16:54:34 -0600

    player movement

--- Player/player.gd
+++ Player/player.gd
@@ -1,1 +1,28 @@
 extends CharacterBody2D
+
+@export var speed = 400
+@export var friction = 6
+@export var acceleration = 5
+
+func _physics_process(delta):
+	player_movement(delta)
+
+func get_input():
+	var input = Vector2()
+	if Input.is_action_pressed('right'):
+		input.x += 1
+	if Input.is_action_pressed('left'):
+		input.x -= 1
+	if Input.is_action_pressed('down'):
+		input.y += 1
+	if Input.is_action_pressed('up'):
+		input.y -= 1
+	return input
+
+func player_movement(delta):
+	var direction = get_input()
+	if direction.length() > 0:
+		velocity = velocity.lerp(direction.normalized() * speed, acceleration * delta)
+	else:
+		velocity = velocity.lerp(Vector2.ZERO, friction * delta)
+	move_and_slide()
