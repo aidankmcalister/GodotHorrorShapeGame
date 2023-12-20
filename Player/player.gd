2d503d5bd9eab44952442c0f8d2ece8edd5873a8
Aidan <aidankmcalister@gmail.com>
2023-12-20 00:36:15 -0600

    player changes

--- Player/player.gd
+++ Player/player.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
 @export var speed = 400
-@export var friction = 8
+@export var friction = 7
 @export var acceleration = 5
+var targetRotation = 0.0
+var rotationSpeed = 5.0 # Adjust the speed as needed
 
 func _physics_process(delta):
 	player_movement(delta)
 
-func get_input():
+func get_input(delta):
 	var input = Vector2()
 	if Input.is_action_pressed('right'):
 		input.x += 1
@@ -20,7 +22,7 @@ func get_input():
 	return input
 
 func player_movement(delta):
-	var direction = get_input()
+	var direction = get_input(delta)
 	if direction.length() > 0:
 		velocity = velocity.lerp(direction.normalized() * speed, acceleration * delta)
 	else:
