4773dd8868dc881e78e5bf763cd1c0671d9b38dc
Aidan <aidankmcalister@gmail.com>
2024-01-08 22:06:35 -0600

    Light ray working

--- Player/player.gd
+++ Player/player.gd
@@ -124,7 +124,7 @@ func _on_dash_duration_timeout():
 
 func change_flashlight_lvl(delta):
 	flashlight_time_passed += delta
-	#print(flashlight_level)
+	print(flashlight_level)
 	if is_in_light:
 		flashlight_level += flashlight_change_rate
 		flashlight_level = clamp(flashlight_level, 0, 100)
@@ -140,41 +140,14 @@ func change_flashlight_lvl(delta):
 func light_raycast():
 	var current_closest_distance = 1000
 	var current_closest_light_node = null
+	
 	for i in global.LIGHTS.size():
 		if global.LIGHTS[i].global_position.distance_to(global_position) <  current_closest_distance:
 			current_closest_distance = global.LIGHTS[i].global_position.distance_to(global_position)
 			current_closest_light_node = global.LIGHTS[i]
-			light_ray.target_position = global.LIGHTS[i].global_position - light_ray.global_position
+			light_ray.target_position = (global.LIGHTS[i].global_position - light_ray.global_position).limit_length(100)
+			
 	if light_ray.is_colliding() and light_ray.get_collider().is_in_group("light"):
-		print("colliding with light")
 		is_in_light = true
 	else:
 		is_in_light = false
-
-func get_closest_from_array(array: Array):
-	pass
-	#vector2.distance_to(array of vectors[index]) will work
-	#yup what @expu mentioned, just loop through the list if the distance_to is less than your current shortest stash the index and the new shortest distance
-	#first time through just auto stash the first values because you know it starts as the shortest and everything else has to try and beat it
-	#can just use a for loop to iterate
-	#Use Array.sort() and sort based on the distance 
-	#Pick the first element of the array to get the closest
-
-#func light_raycast():
-	#pass
-	#if light_ray.is_colliding() and light_ray.get_collider().is_in_group("light"):
-		#print("colliding with light")
-		#is_in_light = true
-	#else:
-		#is_in_light = false
-
-#func closest_target(targets: Array):  
-	#var closest
-	#var shortest_distance = 10000.0
-	#for target in targets:
-		#var distance = target.global_position.distance_to(global_position)
-		#if  distance < shortest_distance:  
-			#shortest_distance = distance  
-			#closest= target  
-	#print("closest target is: ", closest)
-	#return closest.global_position
